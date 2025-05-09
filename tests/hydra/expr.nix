61deeaa8284ea55acb51a34493095bf46bc7ae42
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-05-09 16:35:45 +0200

    Correct luks for hydra

--- tests/hydra/expr.nix
+++ tests/hydra/expr.nix
@@ -8,6 +8,7 @@ eval {
       device = "/dev/disk/by-id/ata-CT2000MX500SSD1_2039E4B20F95";
       name = ""; # legacy reasons, properly name on next format
       withBoot = true;
+      withLuks = false;
     } ];
     stateVersion = "1.1";
   };
